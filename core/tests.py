5aee34aff2f10f46b8d9ad52b559112b7fdcc012
lhckb <luis.carv.cruz@gmail.com>
2022-05-04 16:30:51 -0300

    write E2E tests for first delivery

--- core/tests.py
+++ core/tests.py
@@ -1,15 +1,12 @@
-from socket import if_indextoname
 from django.test import LiveServerTestCase
-from django.test import TestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class TestConnectART(LiveServerTestCase):
 
-  def testlogin(self):
+  def testRegister(self):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
 
@@ -28,6 +25,75 @@ class TestConnectART(LiveServerTestCase):
     password.send_keys('senhalonga123')
     passw_conf.send_keys('senhalonga123')
     submit.click()
+
+    assert "ConnectART | Login" in driver.title
+
+  def testLogin(self):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(self.live_server_url + '/register/')
+
+    username = driver.find_element_by_name('username')
+    email = driver.find_element_by_name('email')
+    password = driver.find_element_by_name('password')
+    passw_conf = driver.find_element_by_name('confirmPassw')
+    submit = driver.find_element_by_name('submitBtn')
+
+    username.send_keys('luis')
+    email.send_keys('luiscruz@gmail.br')
+    password.send_keys('senhalonga123')
+    passw_conf.send_keys('senhalonga123')
+    submit.click()
+
+    assert "ConnectART | Login" in driver.title
+
+    username = driver.find_element_by_name('username')
+    password = driver.find_element_by_name('password')
+    submit = driver.find_element_by_name('submitBtn')
+
+    username.send_keys('luis')
+    password.send_keys('senhalonga123')
+    submit.click()
+
+    assert "ConnectART | Perfil" in driver.title
+
+  def testDelAccount(self):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    driver.get(self.live_server_url + '/register/')
+
+    username = driver.find_element_by_name('username')
+    email = driver.find_element_by_name('email')
+    password = driver.find_element_by_name('password')
+    passw_conf = driver.find_element_by_name('confirmPassw')
+    submit = driver.find_element_by_name('submitBtn')
+
+    username.send_keys('luis')
+    email.send_keys('luiscruz@gmail.br')
+    password.send_keys('senhalonga123')
+    passw_conf.send_keys('senhalonga123')
+    submit.click()
+
+    assert "ConnectART | Login" in driver.title
+
+    username = driver.find_element_by_name('username')
+    password = driver.find_element_by_name('password')
+    submit = driver.find_element_by_name('submitBtn')
+
+    username.send_keys('luis')
+    password.send_keys('senhalonga123')
+    submit.click()
+
+    assert "ConnectART | Perfil" in driver.title
+
+    delBtn = driver.find_element_by_id('delAccount')
+    delBtn.click()
 
     assert "ConnectART | Login" in driver.title
 
