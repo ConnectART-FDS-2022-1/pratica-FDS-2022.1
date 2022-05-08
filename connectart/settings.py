854af6c10e9184d676ce2066caacae24ae9402e6
lhckb <luis.carv.cruz@gmail.com>
2022-05-08 10:19:03 -0300

    edit settings.py

--- connectart/settings.py
+++ connectart/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'connectart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'connectart.sqlite3',
     }
 }
 
